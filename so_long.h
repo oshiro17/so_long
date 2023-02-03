62f5787d9720410898bd82fc863220a070f2cb5d
oshiro17 <nonokapiano@icloud.com>
2023-02-04 08:53:11 +0900

    CLUSTER環境の時、キーコードを定義

--- so_long.h
+++ so_long.h
@@ -10,11 +10,23 @@
 # include "get_next_line/get_next_line.h"
 # include "libft/libft.h"
 // キーボードの定義
+
+#define CLUSTER
+
+#ifdef CLUSTER
+#  define ESC 53
+#  define W 13
+#  define A 0
+#  define S 1
+#  define D 2
+# else
 #define ESC 65307
 #define W 119
 #define A 97
 #define S 115
 #define D 100
+# endif
+
 // 画像の定義
 #define FLOOR "./img/Floor.xpm"
 #define CHARA "./img/human_2.xpm"
@@ -24,4 +36,5 @@
 #define EXIT "./img/exit.xpm"
 int		main(int argc, char **argv);
 void	put_error_exit(char *error_str);
+void	check_error_map(char *map);
 #endif
