e8eb160f3f1154065bafa393562fc908cd2527f3
oshiro17 <nonokapiano@icloud.com>
2023-02-19 16:27:00 +0900

    add move_player_utils

--- key_press.c
+++ key_press.c
@@ -1,4 +1,27 @@
 #include "so_long.h"
+void move_player_utils(t_map **map, int go_x, int go_y, int y, t_info *info)
+{
+	if ((*map)->line[go_x] != '1')
+	{
+		if ((*map)->line[go_x] == 'E' && info->get_c == info->c_num)
+			destroy_win(info);
+		else if ((*map)->line[go_x] == 'E')
+			return;
+		else
+		{
+			if ((*map)->line[go_x] == 'C')
+				info->get_c = info->get_c + 1;
+			(*map)->line[go_x] = 'P';
+			if (y == 1)
+				(*map) = (*map)->pre;
+			if (y == -1)
+				(*map) = (*map)->next;
+			(*map)->line[info->start_x] = '0';
+			info->start_x = go_x;
+			info->start_y = go_y;
+		}
+	}
+}
 
 void move_player(t_info *info, int x, int y)
 {
@@ -16,27 +39,9 @@ void move_player(t_info *info, int x, int y)
 		map = map->next;
 		i++;
 	}
-	if (map->line[go_x] != '1')
-	{
-		if (map->line[go_x] == 'E' && info->get_c == info->c_num)
-			destroy_win(info);
-		else if (map->line[go_x] == 'E')
-			return;
-		else
-		{
-			if (map->line[go_x] == 'C')
-				info->get_c = info->get_c + 1;
-			map->line[go_x] = 'P';
-			if (y == 1)
-				map = map->pre;
-			if (y == -1)
-				map = map->next;
-			map->line[info->start_x] = '0';
-			info->start_x = go_x;
-			info->start_y = go_y;
-		}
-	}
+	move_player_utils(&map, go_x, go_y, y, info);
 }
+
 
 int key_press(int keycode, t_info *info)
 {
@@ -50,11 +55,10 @@ int key_press(int keycode, t_info *info)
 		move_player(info, 0, 1);
 	else if (keycode == D)
 		move_player(info, 1, 0);
-	if (keycode == ESC || keycode == W || keycode == A
-			|| keycode == S || keycode == D)
+	if (keycode == ESC || keycode == W || keycode == A || keycode == S || keycode == D)
 	{
-		info->count = info->count +1;
-		ft_printf("count:%d\n",info->count);
+		info->count = info->count + 1;
+		ft_printf("count:%d\n", info->count);
 	}
 	return (0);
 }
